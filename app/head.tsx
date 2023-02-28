6c5f2c6434550278b76d233e11e65f408db754c5
Shahriar1992 <satowhid8@gmail.com>
2023-02-28 23:33:01 +0600

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Chat GPT Messanger</title>
+      <meta
+        content="width=device-width,
+        initial-scal=1"
+        name="viewport"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
